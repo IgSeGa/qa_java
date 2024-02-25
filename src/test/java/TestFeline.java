042f4fb6050321933e558e65b00ed0d3400d5228
igavrilin <igavrilin@rbc.ru>
2024-02-25 13:55:40 +0300

    Sprint_6

--- src/test/java/TestFeline.java
+++ src/test/java/TestFeline.java
@@ -2,12 +2,10 @@ import com.example.Feline;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TestFeline {
